01a4f1d36d20174148444554ca98e983d6748781
Shlok Singh <139863285+Shlok2307@users.noreply.github.com>
2025-03-22 18:42:05 +0530

    Update st.py

--- st.py
+++ st.py
@@ -24,7 +24,7 @@ period = n_years * 365
 
 @st.cache_data
 def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
+    data = yf.download(ticker, START, TODAY,multi_level_index=False)
     data.reset_index(inplace=True)
     return data
 
